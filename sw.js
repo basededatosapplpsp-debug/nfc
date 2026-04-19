732c042782213a7219e0f2f9b34641b82d9bed32
basededatosapplpsp-debug <basededatosapplpsp@gmail.com>
2026-04-19 16:53:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ===== Service Worker: cache offline + control total ===== */
 
-const CACHE_NAME = "asistencia-v5"; // ⬅️ sube versión cuando cambies algo
+const CACHE_NAME = "asistencia-v6"; // ⬅️ sube versión cuando cambies algo
 const ASSETS = [
   "./",
   "./index.html",
