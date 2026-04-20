403637588af6a876517480e4b81c296f45315029
basededatosapplpsp-debug <basededatosapplpsp@gmail.com>
2026-04-20 18:29:56 -0500

    Update service worker to version v33

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ===== Service Worker: cache offline + control total ===== */
 
-const CACHE_NAME = "asistencia-v32"; // ⬅️ sube versión cuando cambies algo
+const CACHE_NAME = "asistencia-v33"; // ⬅️ sube versión cuando cambies algo
 const ASSETS = [
   "./",
   "./index.html",
