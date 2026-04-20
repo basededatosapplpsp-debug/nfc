d2d2eda5acaf0e32eb0cabf8e61145b495f060b4
basededatosapplpsp-debug <basededatosapplpsp@gmail.com>
2026-04-19 20:57:45 -0500

    Update service worker to version v22

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ===== Service Worker: cache offline + control total ===== */
 
-const CACHE_NAME = "asistencia-v21"; // ⬅️ sube versión cuando cambies algo
+const CACHE_NAME = "asistencia-v22"; // ⬅️ sube versión cuando cambies algo
 const ASSETS = [
   "./",
   "./index.html",
